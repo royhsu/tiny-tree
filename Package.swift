8a7ce0ab6c843ff513990523f6deb26db94437bf
Roy Hsu <roy.hsu@tinyworld.cc>
2020-04-24 21:24:36 +0800

    rename framework.

--- Package.swift
+++ Package.swift
@@ -1,16 +1,17 @@
 // swift-tools-version:5.2
+
 import PackageDescription
 
 let package = Package(
-  name: "TreeUI",
+  name: "TinyTree",
   platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
   products: [
-    .library(name: "TreeCore", targets: ["TreeCore"]),
-    .library(name: "TreeUI", targets: ["TreeUI"]),
+    .library(name: "TinyTreeCore", targets: ["TinyTreeCore"]),
+    .library(name: "TinyTreeUI", targets: ["TinyTreeUI"]),
   ],
   targets: [
-    .target(name: "TreeCore"),
-    .target(name: "TreeUI", dependencies: ["TreeCore"]),
-    .testTarget(name: "TreeUITests", dependencies: ["TreeUI"]),
+    .target(name: "TinyTreeCore"),
+    .target(name: "TinyTreeUI", dependencies: ["TinyTreeCore"]),
+    .testTarget(name: "TinyTreeUITests", dependencies: ["TinyTreeUI"]),
   ]
 )
